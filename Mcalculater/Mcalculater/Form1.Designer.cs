c27b144da76f77adb47b98e0bc0651f2e92e8f87
xuedinggeng <daydayupkevin01@outlook.com>
2020-06-30 21:56:17 +0800

    计算器代码

---
+++ Mcalculater/Mcalculater/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Mcalculater
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBoxOp2 = new System.Windows.Forms.TextBox();
+            this.textBoxOp1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(51, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(591, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(373, 31);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(193, 362);
+            this.panel3.TabIndex = 1;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(39, 296);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "=";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(35, 229);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(39, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "x";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(35, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(23, 19);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(308, 387);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.labelResult);
+            this.panel5.Location = new System.Drawing.Point(11, 24);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(250, 101);
+            this.panel5.TabIndex = 3;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(12, 9);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(53, 20);
+            this.labelResult.TabIndex = 0;
+            this.labelResult.Text = "label2";
+            this.labelResult.Click += new System.EventHandler(this.labelResult_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBoxOp2);
+            this.panel4.Controls.Add(this.textBoxOp1);
+            this.panel4.Location = new System.Drawing.Point(23, 194);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(241, 108);
+            this.panel4.TabIndex = 1;
+            // 
+            // textBoxOp2
+            // 
+            this.textBoxOp2.Location = new System.Drawing.Point(23, 68);
+            this.textBoxOp2.Name = "textBoxOp2";
+            this.textBoxOp2.Size = new System.Drawing.Size(125, 27);
+            this.textBoxOp2.TabIndex = 1;
+            // 
+            // textBoxOp1
+            // 
+            this.textBoxOp1.Location = new System.Drawing.Point(23, 13);
+            this.textBoxOp1.Name = "textBoxOp1";
+            this.textBoxOp1.Size = new System.Drawing.Size(125, 27);
+            this.textBoxOp1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBoxOp2;
+        private System.Windows.Forms.TextBox textBoxOp1;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Mcalculater/Mcalculater/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mcalculater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x * y;
+            this.labelResult.Text = z.ToString();
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x + y;
+            this.labelResult.Text = z.ToString();
+        }
+
+        private void labelResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x - y;
+            this.labelResult.Text = z.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x / y;
+            this.labelResult.Text = z.ToString();
+        }
+    }
+}
